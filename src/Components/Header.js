625457d8e1075aed598cf39ca3b0b27ca8189954
Neha Joshi <nehajoshigkp@gmail.com>
2021-11-18 01:14:56 +0530

    navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,16 +5,16 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 function Home() {
     return (
         <div>
-            <Navbar bg="dark" variant="dark">
+            <Navbar className="header-bg" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Navbar.Brand href="#home"><i class="fa fa-globe" aria-hidden="true"></i> Ecommerce</Navbar.Brand>
                     <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
                     </Nav>
                     <Nav>
-                    <Nav.Link href="#pricing">Cart</Nav.Link>
+                    <Nav.Link href="#pricing"><i class="fa fa-cart-plus" aria-hidden="true"></i></Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
